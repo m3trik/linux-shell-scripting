d44a3174e334d06b0e18464f3c83fefe3144f77a
m3trik <m3trik@live.com>
2021-06-23 14:11:06 -0400

    -

--- set-permissions-array.sh
+++ set-permissions-array.sh
@@ -4,14 +4,12 @@
 # chmod does not follow symlinks.
 
 
-
 #globals
 PM="0777"
 ROOTDIR="/mnt/Storage"
 
 OWNER="admin"
 GROUP="admin"
-
 
 #directories
 declare -a DIRECTORIES=(
@@ -28,17 +26,27 @@ declare -a DIRECTORIES=(
 	"Code/Linux/_scripts"
 )
 
+LEN=${#DIRECTORIES[@]}
+PROGRESS=0
 
-PROGRESSBAR=""
-for i in "${DIRECTORIES[@]}"
-do
+for i in "${DIRECTORIES[@]}"; do
 	clear
 	echo "Setting ownership|permissions for $ROOTDIR/$i .."
-	PROGRESSBAR="$PROGRESSBAR=="
-	echo $PROGRESSBAR
 
 	sudo chown -R $OWNER:$GROUP "$ROOTDIR/$i"
 	sudo chmod -R $PM "$ROOTDIR/$i"
+
+	#calculate percentage and show progress bar:
+	PROGRESS=$((PROGRESS + 1))
+	PERCENT=$(echo "scale=1; $PROGRESS / $LEN * 100" | bc)
+	#PROGRESSBAR="=="
+	#for ((i = 0; i < PROGRESS; i++)); do
+    	#	PROGRESSBAR=$PROGRESSBAR"=="
+	#done
+
+	echo -ne " "
+	echo $PERCENT"%" #$PROGRESSBAR
+
 done
 
 
